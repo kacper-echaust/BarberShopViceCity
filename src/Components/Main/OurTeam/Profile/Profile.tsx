694fe5b45fdf0d3bb1b88605b277524bebd6fd85
Kacper Echaust <k9echaust@gmail.com>
2024-04-15 07:39:21 +0200

    delete comments and unnecesary imports

--- src/Components/Main/OurTeam/Profile/Profile.tsx
+++ src/Components/Main/OurTeam/Profile/Profile.tsx
@@ -1,7 +1,6 @@
 import css from './Profile.module.css'
-import classNames from 'classnames'
 
-import { CSSProperties, ReactNode, useState } from 'react'
+import { CSSProperties, ReactNode } from 'react'
 import { ServiceList } from './ServiceList/ServiceList'
 
 type ProfileType = {
@@ -13,29 +12,6 @@ type ProfileType = {
 }
 
 const Profile = ({ name, photoLink, className, children, style }: ProfileType) => {
-	// const [scrollActive, setScrollActive] = useState({ firstProfile: false, secondProfile: false })
-
-	// const scrollAnimation = () => {
-	// 	if (window.scrollY > 650) {
-	// 		setScrollActive({ ...scrollActive, firstProfile: true })
-	// 	}
-	// 	if (window.scrollY > 1100) {
-	// 		setScrollActive({ firstProfile: true, secondProfile: true })
-	// 	}
-	// }
-
-	// window.addEventListener('scroll', scrollAnimation)
-
-	const profileClasses = classNames(css.profileContainer, className)
-
-	// const firstProfileClasses = classNames(profileClasses, css.firstProfile, {
-	// 	[css.firstProfileAnimation]: scrollActive.firstProfile,
-	// })
-
-	// const secondProfileClasses = classNames(profileClasses, css.secondProfile, {
-	// 	[css.secondProfileAnimation]: scrollActive.secondProfile,
-	// })
-
 	return (
 		<>
 			<div className={css.profileContainer} style={style}>
@@ -46,16 +22,6 @@ const Profile = ({ name, photoLink, className, children, style }: ProfileType) =
 					{children}
 				</div>
 			</div>
-			{/* <div style={style}>
-					<div className={css.descriptionContainer}>
-						<h2>{name}</h2>
-						<ServiceList />
-						
-					</div>
-					<div className={css.imageContainer}>
-						<img className={css.image} src={src} alt='Paulina' />
-					</div>
-				</div> */}
 		</>
 	)
 }
